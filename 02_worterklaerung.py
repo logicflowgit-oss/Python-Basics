3ca562a9c3bdc9d4c55211f46d88214da7f59fe9
logicflowgit-oss <logicflow.git@gmail.com>
2026-07-16 20:19:42 +0200

    02_worterklaerung.py
    Erklärung der Datentypen in Python.

---
+++ 02_worterklaerung.py
@@ -0,0 +1,14 @@
+#ist eine Variable die Alice enthaelt, die als normales Wort vom Programm angesehen wird
+name = "Alice"
+#ist eine Variable die 30 enthaelt, die eine Ausführung ausführen kann
+Age = 30
+#ist eine Variable die 5.7 enthaelt, die eine Ausführunf ausführen kann
+height = 5.7
+#ist eine Variable die True enhaelt, die eine Ausführung ausführen kann
+is_Student = True
+
+#es holt das Wort ider die Zahl was in der Variuable ist durch {} ist und beschreibt den type des Wortes
+print(f" Name: {name} (type:{type(name)})")
+print(f" Name: {Age} (type:{type(Age)})")
+print(f" Height: {height} (type:{type(height)})")
+print(f" is Student: {is_Student} (type:{type(is_Student)})")
