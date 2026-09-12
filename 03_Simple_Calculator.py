db210972ee37209f7b22b22161dde5f034ee5f09
logicflowgit-oss <logicflow.git@gmail.com>
2026-09-12 22:07:24 +0200

    Add files via upload

---
+++ 03_Simple_Calculator.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+
+sum_result = num1 + num2
+print(f"Sum:{sum_result}")
+
+diff_result = num1 - num2
+print(f"Difference:{diff_result}")
+
+div_result = num1 / num2
+print(f"Division:{div_result}")
+
+prod_result = num1 * num2
+print(f"Product:{prod_result}")
